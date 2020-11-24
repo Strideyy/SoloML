f1dc7669fb149c131be7d6384c4e30b971982288
Strider <john16ramsay@gmail.com>
2020-11-24 20:06:58 +0000

    Added test set creation.

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 # Press Shift+F10 to execute
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-##
+# IMPORTS #
 import os
 import tarfile
-from six.moves import urllib
+import numpy as np
 import pandas as pd
+from six.moves import urllib
+from zlib import crc32
 
 
+# Path to fetch data
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
 
+# Data fetching
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
@@ -27,3 +31,24 @@ def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read.csv(csv_path)
 
+
+# Test set creation
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio()))
+    housing = load_housing_data()
+    housing_with_id = housing.reset_index()
+    train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+    return data.loc[~in_test_set], data.loc[in_test_set]
